779860cc0dc4dbf89c718e68d68c10742f24b069
Ismael <isghanim@gmail.com>
2022-05-08 23:47:28 -0400

    Tweak Open Graph properties

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -5,11 +5,17 @@
     <meta name="description"
           content="Too bad we can't be with you in person (yet), but expect something special when you're back!">
     {{-- OpenGraph properties --}}
-    <meta name="og:image"
-          content="https://ichef.bbci.co.uk/news/976/cpsprodpb/3878/production/_106165441_bigsean976.jpg">
+    <meta name="og:site_name" content="Digital gift wrap">
+    <meta name="og:title" content="Happy Birthday, Shawnee!"/>
+    <meta name="og:description"
+          content="Too bad we can't be with you in person (yet), but expect something special when you're back!"/>
+    <meta name="og:image:secure_url" itemprop="image"
+          content="https://cdn.shopify.com/s/files/1/0310/4781/1210/products/charcoal_512x512.png?v=1584462628">
+    <meta name="og:type" content="website"/>
     {{-- TODO: add Twitter properties and more OpenGraph properties --}}
 
     <meta http-equiv="refresh"
           content="0; URL=https://isghanim.notion.site/Happy-Birthday-Roya-2c2784c625a54d99bd6535ea134af88e">
 </head>
+<body></body>
 </html>
