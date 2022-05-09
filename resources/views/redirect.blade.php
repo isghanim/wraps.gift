925ba03b280e800a7a51b2812e2285aa2532fe23
Ismael <isghanim@gmail.com>
2022-05-08 23:35:58 -0400

    Try moving refresh redirect to bottom

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta http-equiv="refresh"
-          content="0; URL=https://isghanim.notion.site/Happy-Birthday-Roya-2c2784c625a54d99bd6535ea134af88e">
-
     <title>Happy Birthday, Shawnee!</title>
     <meta name="description"
           content="Too bad we can't be with you in person (yet), but expect something special when you're back!">
@@ -14,5 +8,8 @@
     <meta name="og:image"
           content="https://ichef.bbci.co.uk/news/976/cpsprodpb/3878/production/_106165441_bigsean976.jpg">
     {{-- TODO: add Twitter properties and more OpenGraph properties --}}
+
+    <meta http-equiv="refresh"
+          content="0; URL=https://isghanim.notion.site/Happy-Birthday-Roya-2c2784c625a54d99bd6535ea134af88e">
 </head>
 </html>
