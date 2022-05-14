c97faf7cd13d5e0fb8d94dfe845324a39cff1847
Ismael <isghanim@gmail.com>
2022-05-14 01:51:42 -0400

    Tweak description

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -13,7 +13,7 @@ class RedirectController extends Controller
 
         return view('redirect', [
             'title' => 'Happy Birthday, Shawnee!',
-            'description' => "We have a very special meowssage for you from someone of a... feline disposition.",
+            'description' => "We have a very special meowssage for you.",
             'imageUrl' => 'https://cdn.shopify.com/s/files/1/0310/4781/1210/products/charcoal_512x512.png?v=1584462628',
             'url' => 'https://www.notion.so/isghanim/Happy-Birthday-Shawnee-2172d14aebea4ae9b323f065405c289b',
         ]);
